747c365a8509c04cad44e961dbe7e55d4ffc268c
Aidan Ferrara <anferrara@umass.edu>
2022-11-21 12:55:32 -0500

    parameter checks

--- hw7-fall-22/src/fetchUniversities.js
+++ hw7-fall-22/src/fetchUniversities.js
@@ -1,4 +1,16 @@
 export function fetchUniversities(query) {
+  //check to make sure parameter is there
+  if (query === undefined) {
+    return Promise.reject(new Error("No query provided."));
+  }
+  //check to make sure parameter is a string
+  if (typeof query !== "string") {
+    return Promise.reject(new Error("Query is not a string."));
+  }
+  //check to make sure parameter is not empty
+  if (query === "") {
+    return Promise.reject(new Error("Query is empty."));
+  }
   return fetch(`https://university-web-api.herokuapp.com/search?name=${query}`)
     .then((response) => {
       if (response.ok) {
