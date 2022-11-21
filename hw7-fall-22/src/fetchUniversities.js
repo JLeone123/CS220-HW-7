f22128d339bf87457214ac19b32de921d15fc904
Aidan Ferrara <anferrara@umass.edu>
2022-11-21 12:44:34 -0500

    response.ok

--- hw7-fall-22/src/fetchUniversities.js
+++ hw7-fall-22/src/fetchUniversities.js
@@ -1,6 +1,12 @@
 export function fetchUniversities(query) {
   return fetch(`https://university-web-api.herokuapp.com/search?name=${query}`)
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(new Error("No results found."));
+      }
+    })
     .then((json) => {
       let universities = [];
       for (let i = 0; i < json.length; i++) {
