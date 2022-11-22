8563aaf0d9f8f7eba83a37f4e2d09f6e3501d296
Aidan Ferrara <anferrara@umass.edu>
2022-11-22 11:01:14 -0500

    error handling

--- hw7-fall-22/src/fileUtility.js
+++ hw7-fall-22/src/fileUtility.js
@@ -5,11 +5,12 @@ export function writeToJSONFile(path, data) {
 }
 
 export function readFromJSONFile(path) {
-  // TODO
   const data = {};
   return readFile(path, {encoding:"utf8"}).then((results) => {
     if(!data[results]){
       data[results] =  JSON.parse(results);
     }
+  }).catch((err) => {
+    console.log(err);
   });
 }
