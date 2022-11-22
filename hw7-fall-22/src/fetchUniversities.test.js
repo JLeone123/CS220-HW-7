9739bac71ef332659661d8f0750691bd84eaef7c
Aidan Ferrara <anferrara@umass.edu>
2022-11-22 13:02:43 -0500

    fetchUniversities tests

--- hw7-fall-22/src/fetchUniversities.test.js
+++ hw7-fall-22/src/fetchUniversities.test.js
@@ -10,3 +10,31 @@ test("fetchUniversities follows type specification", () => {
     assert(result.every((x) => typeof x === "string")); // Assert each element in the array is a string
   });
 });
+
+test("fetchUniversities outputs the correct number of results for a query with multiple results", () => {
+  const promise = fetchUniversities("University of Massachusetts");
+  return promise.then((result) => {
+    assert(result.length === 4);
+  });
+});
+
+test("fetchUniversities outputs the correct number of results for a query with a single result", () => {
+  const promise = fetchUniversities("University of Massachusetts Amherst");
+  return promise.then((result) => {
+    assert(result.length === 1);
+  });
+});
+
+test("fetchUniversities outputs the correct number of results for a query with no results", () => {
+  const promise = fetchUniversities("University of California, Los Angeles");
+  return promise.then((result) => {
+    assert(result.length === 0);
+  });
+});
+
+test("fetchUniversities rejects a query that is undefined", () => {
+  const promise = fetchUniversities(undefined);
+  return promise.catch((result) => {
+    assert(result === "No query provided.");
+  });
+});
