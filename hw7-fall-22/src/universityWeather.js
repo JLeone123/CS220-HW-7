15ebe29aa85dccedd3b3cd4385051beae9d1f348
JLeone123 <jsalleo13@gmail.com>
2022-11-22 06:55:05 -0500

    Work on task 4

--- hw7-fall-22/src/universityWeather.js
+++ hw7-fall-22/src/universityWeather.js
@@ -3,7 +3,32 @@ import { fetchLongitudeAndLatitude } from "./fetchLongitudeAndLatitude.js";
 import { fetchUniversities } from "./fetchUniversities.js";
 
 export function fetchUniversityWeather(query) {
-  // TODO  
+  // TODO
+  let totalAvg = 0;
+  let obj = {};
+  let count = 0;
+  let sum = 0;
+
+  return fetchUniversities(query).then((arr) => {
+    return arr.map((x) => {
+      return fetchLongitudeAndLatitude(x).then((y) => {
+        return fetchCurrentWeather(y.lon, y.lat).then((z) => {
+          count = count + 1;
+
+          z["temperature_2m"].map((x) => (sum += x));
+          obj[x] = sum / z["temperature_2m"].length;
+          totalAvg += obj[x];
+
+          sum = 0;
+
+          if (count === arr.length) {
+            obj["totalAverage"] = totalAvg / arr.length;
+            return obj;
+          }
+        });
+      });
+    });
+  });
 }
 
 export function fetchUMassWeather() {
@@ -12,5 +37,4 @@ export function fetchUMassWeather() {
 
 export function fetchUCalWeather() {
   // TODO
-  
 }
