f05fe53b9c2843e3347c44b35d4a35321c9a4139
Joe <118311235+dracomoory@users.noreply.github.com>
2022-11-22 14:16:42 -0500

    Update fileUtility.js

--- hw7-fall-22/src/fileUtility.js
+++ hw7-fall-22/src/fileUtility.js
@@ -1,11 +1,13 @@
 import { readFile, writeFile } from "node:fs/promises";
 
+// Joe Lebedev
 export function writeToJSONFile(path, data) {
   return writeFile(path, JSON.stringify(data), (err) => {
     if (err) throw err;
   });
 }
 
+// Aidan Ferrara
 export function readFromJSONFile(path) {
   const data = {};
   return readFile(path, { encoding: "utf8" })
